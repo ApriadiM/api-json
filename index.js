10ef85883f47534255025e59ffaadb1b2d4f2eb7
Apriadi <apriadim>
2019-11-13 17:42:34 +0700

    add followers

--- index.js
+++ index.js
@@ -16,4 +16,25 @@ const fetchFollowing = () => {
         console.log(data);
     });
 };
+
+const fetchFollowers = () => {
+    fetch("https://api.github.com/users/apriadim/followers")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        let getFollowers = document.getElementById
+        ("followers-list");
+        data.map(item => {
+            let eachList = document.createElement("img");
+            let textList = document.createTextNode(item.login);
+            eachList.setAttribute("src", item.avatar_url);
+            eachList.appendChild(textList);
+            getFollowers.appendChild(eachList);
+        });
+        console.log(data);
+    });
+};
+
 fetchFollowing();
+fetchFollowers();
